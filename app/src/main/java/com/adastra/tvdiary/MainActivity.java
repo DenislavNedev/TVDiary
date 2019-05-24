ee2ac9bc59e5c46bf83909c6ba8a890f783b89b2
DenislavNedev <dnedev98@gmail.com>
2019-05-24 11:18:06 +0300

    Added Retrofit and Butter Knife. Crated new classes
    Implemented Retrofit and Butter Knife to the project. Also created Show class for the every show, which will be generated from the Api. Created JsonPlaceHolderApi interface using Retrofit, to get information from the Api

--- app/src/main/java/com/adastra/tvdiary/MainActivity.java
+++ app/src/main/java/com/adastra/tvdiary/MainActivity.java
@@ -2,12 +2,68 @@ package com.adastra.tvdiary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.POST;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.textViewResult)
+    TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.tvmaze.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Show>> call = jsonPlaceHolderApi.getShows();
+
+        call.enqueue(new Callback<List<Show>>() {
+            @Override
+            public void onResponse(Call<List<Show>> call, Response<List<Show>> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Show> shows = response.body();
+
+                for(Show show : shows){
+                    String contet ="";
+                    contet += "Name:" + show.getName() + "\n";
+                    contet += "Genres:" + show.getGenres() + "\n";
+                   // contet += "Rating:" + show.getRating() + "\n";
+                    contet += "Summary:" + show.getSummary() + "\n\n";
+
+                    textViewResult.append(contet);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Show>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/adastra/tvdiary/Show.java
@@ -0,0 +1,27 @@
+package com.adastra.tvdiary;
+
+import java.util.List;
+
+public class Show {
+    private String name;
+    private List<String> genres;
+    //private String rating;
+    private String summary;
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+
+    //public String getRating() {
+    //     return rating;
+    //}
+
+    public String getSummary() {
+        return summary;
+    }
+}
